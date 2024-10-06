5584926b4b97f0f7e706e1406d74ba2dd5b12410
Alexander Bassov <blackxes.dev@gmail.com>
2024-10-06 17:14:47 +0200

    Updated exports and vite.config

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./core/plugin-base";
+export * from "./core/plugin-loader";
+export * from "./core/plugin-registry";
+
+export * from "./@types/plugin-system.types";
+
+export { default } from "./core/plugin-manager";
