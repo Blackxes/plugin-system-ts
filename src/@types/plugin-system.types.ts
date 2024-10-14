327ef933bcc49d7015a86affc26e9f95724536a6
Alexander Bassov <blackxes.dev@gmail.com>
2024-10-14 16:43:58 +0200

    Removed wusa argument. lol

--- src/@types/plugin-system.types.ts
+++ src/@types/plugin-system.types.ts
@@ -4,7 +4,7 @@
  */
 
 export interface PluginBaseInterface {
-  beforeInit?: (wusa: number) => boolean;
+  beforeInit?: () => boolean;
   init?: (() => boolean) | (() => Promise<boolean>);
   shutdown?: () => void;
   afterInit?: () => boolean;
